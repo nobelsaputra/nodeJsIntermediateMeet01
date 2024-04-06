48bd2fdbcf910495e9297ab994fea9391083b3b9
nobelsaputra53 <150402008+nobelsaputra@users.noreply.github.com>
2024-04-06 12:14:45 +0700

    npm
    belajar node js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node-js-intermidiete
  Learning node js 
+
+hari ini belajar node js dan menginstall sebuah npm dengan nama unique name generator 
+
---
+++ index.js
@@ -0,0 +1,22 @@
+// const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
+
+// const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] }); // big_red_donkey
+
+// const shortName = uniqueNamesGenerator({
+//   dictionaries: [adjectives, animals, colors], // colors can be omitted here as not used
+//   length: 2,
+// }); // big-donkey
+
+// console.log(randomName);
+// console.log(shortName);
+
+const { AnimeWallpaper, AnimeSource } = require('anime-wallpaper');
+const wallpaper = new AnimeWallpaper();
+
+async function main() {
+    const waifu = await wallpaper.search({ title: 'mikasa' }, AnimeSource.ZeroChan);
+    console.log(waifu);
+}
+
+main();
+
